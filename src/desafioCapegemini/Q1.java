2418ac3eef8b1d97447a6a8a94b8b3543fea62c1
Lucas Silva <79523835+guilucasv@users.noreply.github.com>
2022-02-21 00:55:32 -0300

    Add files via upload

--- src/desafioCapegemini/Q1.java
+++ src/desafioCapegemini/Q1.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 public class Q1 {
 
 	public static void main(String[] args) {
-		//Scanner para leitura do valor 
 		Scanner entrada = new Scanner (System.in);
 		int n;
-		String ast = "*";
-		//Leitura do valor digitado pelo usuário e atribuição a variável n
+		System.out.print("Informe um valor inteiro: ");
 		n = entrada.nextInt();
-		//laço de repetição para cada valor do vetor
 		for (int i = 0; i <= n; i++) {
 			System.out.print("\n");
-			//Laço de repetição aninhado com impressão dos valores
 			for(int j = 0; j <= i-1; j++) {
-				System.out.print(ast);
+				System.out.print("*");
 			}
 		}
+		entrada.close();
 	}
 }
+
--- src/desafioCapegemini/Q2.java
+++ src/desafioCapegemini/Q2.java
@@ -1,24 +1,50 @@
 package desafioCapegemini;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 
 public class Q2 {
-	public static void main(String[] args) {
-		//Declaração de variáveis
+	public static int main(String[] args) {
 		Scanner entrada = new Scanner (System.in);
-		String senha;
-		int tamanhoSenha;
-		senha = entrada.nextLine();
+		System.out.print("Informe um valor:");
+		String senha = entrada.nextLine();
+		boolean senhaValida = true;
+     
+        Pattern checkDigito = Pattern.compile("(.)*(\\d)(.)*");
+        boolean containsNumber = checkDigito.matcher(senha).matches();
+
+		Pattern checkMinusculo = Pattern.compile("^(?=.*[a-z]).+$");
+		boolean containsMinusculo = checkMinusculo.matcher(senha).matches();
+
+		Pattern checkMaiusculo = Pattern.compile("^(?=.*[A-Z]).+$");
+		boolean containsMaiusculo = checkMaiusculo.matcher(senha).matches();
+
+		Pattern checkEspecial = Pattern.compile("^[^!@#$%&*()-+].+$");
+		boolean containsEspecial = checkEspecial.matcher(senha).matches();
 		
-		tamanhoSenha = senha.length();
-		//Condição de satição da senha
-		if (tamanhoSenha >= 6 ) {
-			
-			System.out.print("Senha OK");
+
+		if ( senha.length() < 6 ) {
+			System.out.println("Sua senha nao contem 6 caracteres.");
+			senhaValida = false;
+		}else if(containsNumber == false) {
+			System.out.println("E obrigatorio conter um digito em sua senha");
+			senhaValida = false;
+		}else if(containsMinusculo == false) {
+			System.out.println("E obrigatorio conter uma letra minuscula");
+			senhaValida = false;
+		}else if(containsMaiusculo == false) {
+			System.out.println("E obrigatorio conter uma letra maiuscula");
+			senhaValida = false;
+		}else if(containsEspecial == false) {
+			System.out.println("E obrigatorio conter um caracter especial: !@#$%^&*()-+ ");
+			senhaValida = false;
 		}
-		else {
-	
-			System.out.print(6 - tamanhoSenha);
+		entrada.close();
+		if (senhaValida = true) {
+			return 0;
 		}
+		System.out.println("Senha Valida");		
+		return 1;
 	}
 }
 
